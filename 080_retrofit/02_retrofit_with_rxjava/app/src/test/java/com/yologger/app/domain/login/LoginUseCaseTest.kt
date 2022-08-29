d2e40817cd6f6f47d1c8113179c5a701e9d14366
yologger <yologger1013@gmail.com>
2022-08-29 14:35:42 +0900

    [ADD] Retrofit + Rxjava + Clean Architecture 도메인 계층 테스트 코드 추가

---
+++ 080_retrofit/02_retrofit_with_rxjava/app/src/test/java/com/yologger/app/domain/login/LoginUseCaseTest.kt
@@ -0,0 +1,52 @@
+package com.yologger.app.domain.login
+
+import com.yologger.app.domain.repository.AuthRepository
+import com.yologger.app.scheduler.TrampolineSchedulerRule
+import io.reactivex.rxjava3.core.Observable
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
+class LoginUseCaseTest {
+
+    @Mock
+    lateinit var authRepository: AuthRepository;
+
+    lateinit var loginUseCase: LoginUseCase
+
+    @get:Rule
+    val rule = TrampolineSchedulerRule()
+
+    @Before
+    fun setUp() {
+        authRepository = mock(AuthRepository::class.java)
+        loginUseCase = LoginUseCase(authRepository)
+    }
+
+    @Test
+    fun `로그인 성공 테스트`() {
+        // Given
+        val dummyEmail = "dummy@gmail.com"
+        val dummyPassword = "password"
+        val dummyAccessToken = "access_token"
+        val dummyRefreshToken = "refresh_token"
+        val dummyLoginData = LoginData(accessToken = dummyAccessToken, refreshToken = dummyRefreshToken)
+        val dummyLoginResult = LoginResult.SUCCESS(dummyLoginData)
+
+        // When
+        `when`(authRepository.login(anyString(), anyString())).thenReturn(Observable.just(dummyLoginResult))
+
+        // Then
+        val params = LoginUseCase.Params(email = dummyEmail, password = dummyPassword)
+        loginUseCase.execute(params)
+            .test()
+            .assertNoErrors()
+            .assertValue { result -> result is LoginResult.SUCCESS }
+            .assertValue { result -> (result as LoginResult.SUCCESS).data.accessToken == dummyAccessToken }
+            .assertValue { result -> (result as LoginResult.SUCCESS).data.refreshToken == dummyRefreshToken }
+    }
+}
