309bf66f63dee6a2da8ab9f6edb118641faf176b
yologger <yologger1013@gmail.com>
2022-09-06 21:46:37 +0900

    [ADD] 로그인 실패 테스트 코드 추가

--- 080_retrofit/03_retrofit_with_coroutine/app/src/test/java/com/yologger/app/domain/login/LoginUseCaseTest.kt
+++ 080_retrofit/03_retrofit_with_coroutine/app/src/test/java/com/yologger/app/domain/login/LoginUseCaseTest.kt
@@ -46,4 +46,25 @@ class LoginUseCaseTest {
         assertThat((result as LoginResult.Success).data.accessToken).isEqualTo(dummyAccessToken)
         assertThat((result as LoginResult.Success).data.refreshToken).isEqualTo(dummyRefreshToken)
     }
+
+    @Test
+    fun `로그인 실패 테스트 - 잘못된 이메일`() = runBlocking {
+        // Given
+        val dummyEmail = "dummy@gmail.com"
+        val dummyPassword = "password"
+        val dummyAccessToken = "access_token"
+        val dummyRefreshToken = "refresh_token"
+        val dummyLoginData = LoginData(accessToken = dummyAccessToken, refreshToken = dummyRefreshToken)
+        val dummyLoginResult = LoginResult.Failure(LoginError.INVALID_EMAIL)
+
+        // When
+        Mockito.`when`(authRepository.login(anyString(), anyString())).thenReturn(dummyLoginResult)
+
+        // Then
+        val params = LoginUseCase.Params(email = dummyEmail, password = dummyPassword)
+        val result = loginUseCase.execute(params)
+
+        assertThat(result is LoginResult.Failure).isTrue()
+        assertThat((result as LoginResult.Failure).error).isEqualTo(LoginError.INVALID_EMAIL);
+    }
 }
